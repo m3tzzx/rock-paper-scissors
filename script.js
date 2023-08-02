dc80aba4bafd9cd0dd0949ed1fb6b0449fbfedf1
m3tzzx <mtzalqarni@gmail.com>
2023-08-02 03:44:08 +0300

    Add a function to play the game

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // get choice from user
 const playerSelection = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
 // get choice from computer randomly
-const computerSelection = function() {
+const computerSelection = getComputerChoice();
+
+
+function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
         return "rock"
@@ -11,6 +14,17 @@ const computerSelection = function() {
         return "scissors"
     }
 }
+
+
 // start the game between the user and the computer
+function playSingleRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection){
+        return "You are even! Play again"
+    } else if((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
 // if one of them win 
 //          add one point to the winner and show the points
